889bc45c6be610b838a6a6694dc99519b6082f0b
KatrinaWright <katrina.elise.wright@gmail.com>
2023-10-01 19:07:03 -0400

    NewPersonalProject Note_Maker Youtube Scraper

---
+++ noteMakerYT1.py
@@ -0,0 +1,4 @@
+#Youtube Scraper and Meeting Summary maker
+#Katrina Wright - Joy of Coding Academy
+#Used tutorial from brightdata: https://brightdata.com/blog/how-tos/how-to-scrape-youtube-in-python
+
